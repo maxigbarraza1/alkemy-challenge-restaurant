93766416b98e218e56d3926dfad9a72a8ececede
Gabriel M. Barraza <gbarraza@alumnos.exa.unicen.edu.ar>
2022-09-12 20:19:11 -0300

    Cambios finales

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 </p>
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.1.
-# A la carta restaurant
+# A la carta restaurante
 
 1. Clonar el proyecto
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_AUTH_URL: 'http://challenge-react.alkemy.org/',
   API_URL: 'https://api.spoonacular.com/recipes',
-  API_KEY: 'apiKey=60eb9f795c874754a6ccf713fce6144f'
+  API_KEY: 'apiKey='
 };
